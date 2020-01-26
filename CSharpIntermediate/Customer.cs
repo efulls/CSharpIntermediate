c01e1ea8bada58c1c264d6959dd975023f9fa391
efulls <saeful.mahmud@gmail.com>
2020-01-26 12:15:49 +0700

    8. Constructors

---
+++ CSharpIntermediate/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpIntermediate
+{
+    class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+        public Customer(int id)
+            : this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -14,8 +14,20 @@ namespace CSharpIntermediate
         static void Main(string[] args)
         {
             /* Demo Introduction Classes */
-            var person = Person.Parse("John");
-            person.Introduce("Eful");
+            //var person = Person.Parse("John");
+            //person.Introduce("Eful");
+
+            /* Demo Constructors*/
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
