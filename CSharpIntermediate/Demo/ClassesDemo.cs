d8a95cf03ccda70774cdb97200f2dd08e2514d04
efulls <saeful.mahmud@gmail.com>
2020-01-26 10:58:57 +0700

    7. Introduction to Classes

---
+++ CSharpIntermediate/Demo/ClassesDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate.Demo
+{
+    class ClassesDemo
+    {
+        
+        public class Person
+        {
+            public string Name;
+            public void Introduce(string to)
+            {
+                Console.WriteLine("Hi {0}, I am {1}", to, Name);
+            }
+
+            public static Person Parse(string str)
+            {
+                var person = new Person();
+                person.Name = str;
+
+                return person;
+            }
+        }
+    }
+}
--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
+﻿using CSharpIntermediate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CSharpIntermediate.Demo.ClassesDemo;
 
 namespace CSharpIntermediate
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
+            /* Demo Introduction Classes */
+            var person = Person.Parse("John");
+            person.Introduce("Eful");
         }
     }
 }
