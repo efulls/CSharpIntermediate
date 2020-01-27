3269c22499f253921b7bdaae9bb9d658bdda2b99
efulls <saeful.mahmud@gmail.com>
2020-01-27 15:33:54 +0700

    29. Abstract Classes & Members

---
+++ AbstractClasses/Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    public abstract class Shape
+    {
+        public int With { get; set; }
+        public int Height { get; set; }
+
+        public abstract void Draw();
+
+        public void Copy()
+        {
+            Console.WriteLine("Copy shape into clopboard.");
+        }
+
+        public void Select()
+        {
+            Console.WriteLine("Select the shape.");
+        }
+    }
+}
