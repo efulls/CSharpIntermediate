7c6cb75ba169f01973f58541923a5ba8130762a4
efulls <saeful.mahmud@gmail.com>
2020-01-27 10:03:45 +0700

    23. Constructor and Inheritance

---
+++ Constructors/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Car : Vehicle
+    {
+        public Car(string registrationNumber)
+            : base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initializes. {0}", registrationNumber);
+        }
+    }
+}
---
+++ Constructors/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Constructors
+{
+    class Vehicle
+    {
+        private readonly string _registrationNumber;
+        //public Vehicle()
+        //{
+        //    System.Console.WriteLine("Vehicle is being initialized");
+        //}
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+            System.Console.WriteLine("Vehicle is being initialized. {0}", registrationNumber);
+        }
+    }
+}
